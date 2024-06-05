6d4bc7d00f4bcdac88073f43e541eeefaab077a3
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-06-05 20:21:17 +0200

    Integration check

--- langfuse/UnifyLangfuse.py
+++ langfuse/UnifyLangfuse.py
@@ -107,7 +107,7 @@ class UnifyLangfuse:
     def flush(cls):
         cls._langfuse.flush()
 
-    @langfuse_context
+    @lobserve()
     def generate(self, model: str, *args, **kwargs):
         try:
             # Parse the model@provider format
