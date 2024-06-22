a8b8a5aacfffb8d16078b2ca1fa51bb1000e1bf4
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-06-22 21:30:31 +0200

    fix: UnifyLangfuse.initialize

--- langfuse/unify/test_file.py
+++ langfuse/unify/test_file.py
@@ -5,12 +5,13 @@ import os
 from dotenv import load_dotenv
 from langfuse.decorators import observe
 
+
 load_dotenv()
 os.environ["LANGFUSE_PUBLIC_KEY"] = ""
 print(f"LangFuse Enabled: {unify.langfuse_enabled}")
 path = os.environ["PATH"]
 unify.langfuse_secret_key = os.getenv("LANGFUSE_SECRET_KEY")
-unify.langfuse_public_key = os.getenv("LANGFUSE_PK")
+unify.langfuse_public_key = os.getenv("LANGFUSE_PUBLIC_KEY")
 unify.langfuse_host = os.getenv("LANGFUSE_HOST")
 unify_api_key = os.getenv("UNIFY_API_KEY")
 print(f"LangFuse host: {unify.langfuse_host}")
