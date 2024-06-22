f38371f7573eda33ab252481fa47fdadec15b92d
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-06-22 11:32:35 +0200

    fix: UnifyLangfuse.initialize

--- langfuse/unify/__init__.py
+++ langfuse/unify/__init__.py
@@ -1,3 +1,17 @@
-from .unify_integration import unify, Unify, AsyncUnify, ChatBot
+from .unify_integration import (
+    unify,
+    Unify,
+    AsyncUnify,
+    ChatBot,
+    auth_check,
+    _filter_image_data,
+)
 
-__all__ = ["unify", "Unify", "AsyncUnify", "ChatBot"]
+__all__ = [
+    "unify",
+    "Unify",
+    "AsyncUnify",
+    "ChatBot",
+    "auth_check",
+    "_filter_image_data",
+]
