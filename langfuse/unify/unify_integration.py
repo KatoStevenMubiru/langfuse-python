702b3011784d52aa992c7d8293a6b8d9f60f7f77
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-06-23 14:22:14 +0200

    fix: UnifyLangfuse.initialize

--- langfuse/unify/unify_integration.py
+++ langfuse/unify/unify_integration.py
@@ -52,7 +52,6 @@ def _unify_wrapper(func):
             initialize = replacer
 
             def wrapper(wrapped, instance, args, kwargs):
-                print(str(initialize))
                 return func(open_ai_resource, initialize, wrapped, args, kwargs)
 
             return wrapper
